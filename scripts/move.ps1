2eb9c4a9470060837d209b1dd3f02805cf68fe9a
Charly-Hudson <charlydhudson+Github@gmail.com>
2024-08-24 22:25:19 +0100

    update for test

--- scripts/move.ps1
+++ scripts/move.ps1
@@ -1,6 +1,7 @@
 $repoName = Read-Host -Prompt "Whats your repo name"
 
-Get-ChildItem -Path base-plate -Recurse -Directory -File | Move-Item -Destination $repoName
-Get-ChildItem -Path base-plate -Recurse -Directory | Remove-Item
+Get-ChildItem -Path "base-plate" -Recurse -Directory -File | Move-Item -Destination $repoName
+
+# Get-ChildItem -Path "base-plate" -Recurse -Directory | Remove-Item
 
 Write-User"Done"
