e8fb13d92941ea504eb36b15d0ad87338948bb80
Charly-Hudson <charlydhudson+Github@gmail.com>
2024-08-24 22:47:01 +0100

    move fixed just need to finish delte of base-plate

--- scripts/move.ps1
+++ scripts/move.ps1
@@ -1,7 +1,7 @@
-$repoName = Read-Host -Prompt "Whats your repo name"
+$currentPath = Get-Location
 
-Get-ChildItem -Path "base-plate" -Recurse -Directory -File | Move-Item -Destination $repoName
+Get-ChildItem -Path "base-plate" | Move-Item -Destination $currentPath
 
-# Get-ChildItem -Path "base-plate" -Recurse -Directory | Remove-Item
+Get-ChildItem -Path "base-plate" -Recurse -Directory | Remove-Item
 
-Write-User"Done"
+Write-Host "Done"
