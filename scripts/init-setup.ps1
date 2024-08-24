36dfbbc5292052be1572c5653e6e63cc87699d89
Charly-Hudson <charlydhudson+Github@gmail.com>
2024-08-24 23:20:34 +0100

    shouldnt need pages folder now, fixed issue with init-setup.ps1

--- scripts/init-setup.ps1
+++ scripts/init-setup.ps1
@@ -8,8 +8,9 @@ Write-Host "|=======================================|"
 
 $componentType = Read-Host -Prompt "Input Here (Please use lowercase)"
 
-echo > pages/$componentType.html '<!DOCTYPE html>' '<html lang="en">' '   <head>' '      <meta charset="UTF-8">' '      <meta name="viewport" content="width=device-width, initial-scale=1.0">' '      <link rel="stylesheet" href="/assets/styles/global.css">' "      <link rel='stylesheet' href='/assets/styles/$componentType.css'>" '   </head>' '   <body>' ' ' '   </body>' '</html>'
+echo > .\$componentType.html '<!DOCTYPE html>' '<html lang="en">' '   <head>' '      <meta charset="UTF-8">' '      <meta name="viewport" content="width=device-width, initial-scale=1.0">' '      <link rel="stylesheet" href="/assets/styles/global.css">' "      <link rel='stylesheet' href='/assets/styles/$componentType.css'>" '   </head>' '   <body>' ' ' '   </body>' '</html>'
 echo > assets/styles/$componentType.css "*{" "    padding:0;" "    margin:0;" "}"
+
 
 Write-Host "|==================|"
 Write-Host "| $componentType.css made |"
