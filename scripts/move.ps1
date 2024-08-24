bde722e5606f374c7cd350810204acef953189bf
Charly-Hudson <charlydhudson+Github@gmail.com>
2024-08-24 23:15:23 +0100

    finished script and delete with readme

--- scripts/move.ps1
+++ scripts/move.ps1
@@ -2,6 +2,6 @@ $currentPath = Get-Location
 
 Get-ChildItem -Path "base-plate" | Move-Item -Destination $currentPath
 
-Get-ChildItem -Path "base-plate" -Recurse -Directory | Remove-Item
+Remove-Item "base-plate"
 
 Write-Host "Done"
