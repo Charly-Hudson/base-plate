28f0bcdcca51c87aecfbfdfd60cb73da7cb43d11
Charly-Hudson <charlydhudson+Github@gmail.com>
2024-08-25 01:32:02 +0100

    init.ps1 has been beautified

--- scripts/init-setup.ps1
+++ scripts/init-setup.ps1
@@ -1,19 +1,39 @@
 Write-Host "What sort of component do you want to make ?"
 Write-Host "E.G:"
-Write-Host "|=======================================|"
+Write-Host "_________________________________________"
+Write-Host "|             |              |          |"
 Write-Host "| Nav-Bar     |   Footer     | Button   |"
 Write-Host "| Backgrounds |   Cards      | Carosell |"
 Write-Host "| Full Site   |   Services   | More...  |"
-Write-Host "|=======================================|"
+Write-Host "|_______________________________________|"
 
 $componentType = Read-Host -Prompt "Input Here (Please use lowercase)"
 
+Write-Host "______________________"
+Write-Host "|                    |"
+Write-Host "| Creating HTML File |"
+Write-Host "|____________________|"
+
 echo > .\$componentType.html '<!DOCTYPE html>' '<html lang="en">' '   <head>' '      <meta charset="UTF-8">' '      <meta name="viewport" content="width=device-width, initial-scale=1.0">' '      <link rel="stylesheet" href="/assets/styles/global.css">' "      <link rel='stylesheet' href='/assets/styles/$componentType.css'>" '   </head>' '   <body>' ' ' '   </body>' '</html>'
+
+Write-Host "_______________________"
+Write-Host "|                     |"
+Write-Host "|  Creating CSS File  |"
+Write-Host "|_____________________|"
+
 echo > assets/styles/$componentType.css "*{" "    padding:0;" "    margin:0;" "}"
 
+Write-Host "_______________________"
+Write-Host "|                     |"
+Write-Host "| Installing Prettier |"
+Write-Host "|_____________________|"
 
-Write-Host "|==================|"
-Write-Host "| $componentType.css made |"
-Write-Host "| $componentType.html made |"
-Write-Host "|       Done       |"
-Write-Host "|==================|"
+npm update
+npm install prettier
+
+Write-Host "--------------------"
+Write-Host "$componentType.css"
+Write-Host "$componentType.html"
+Write-Host "Prettier Installed"
+Write-Host "Setup Done"
+Write-Host "--------------------"
--- scripts/testing.ps1
+++ scripts/testing.ps1
@@ -1,8 +1,17 @@
-$test = Read-Host -Prompt  "Test?"
+# $test = Read-Host -Prompt  "Test?"
 
-echo > scripts/test/$test.html '<!DOCTYPE html>' '<html lang="en">' '   <head>' '      <meta charset="UTF-8">' '      <meta name="viewport" content="width=device-width, initial-scale=1.0">' '      <link rel="stylesheet" href="/assets/styles/global.css">' "      <link rel='stylesheet' href='/assets/styles/$test.css'>" '   </head>' '   <body>' ' ' '   </body>' '</html>'
-echo > scripts/test/$test.css "*{" "    padding:0;" "    margin:0;" "}"
+# echo > scripts/test/$test.html '<!DOCTYPE html>' '<html lang="en">' '   <head>' '      <meta charset="UTF-8">' '      <meta name="viewport" content="width=device-width, initial-scale=1.0">' '      <link rel="stylesheet" href="/assets/styles/global.css">' "      <link rel='stylesheet' href='/assets/styles/$test.css'>" '   </head>' '   <body>' ' ' '   </body>' '</html>'
+# echo > scripts/test/$test.css "*{" "    padding:0;" "    margin:0;" "}"
 
-Write-Host "|============|"
-Write-Host "| $test Done |"
-Write-Host "|============|"
+# Write-Host "|============|"
+# Write-Host "| $test Done |"
+# Write-Host "|============|"
+
+$componentType = "nav-bar"
+
+Write-Host "--------------------"
+Write-Host "$componentType.css"
+Write-Host "$componentType.html"
+Write-Host "Prettier Installed"
+Write-Host "Setup Done"
+Write-Host "--------------------"
